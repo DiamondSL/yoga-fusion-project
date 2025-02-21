f4a80aaad4ee6b3ad9134fa2d6d0b3f912b05271
Oleh Grechukh <diamondsl.dg9@gmail.com>
2025-02-21 07:51:54 +0200

    local updates

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,30 +3,30 @@ import {Container, Typography} from '@mui/material';
 import { gql, useQuery } from '@apollo/client';
 import {Box} from "@mui/system";
 
-const GET_ARTICLES = gql`
-  query articles {
-    articles {
-      title
-      description
-    }
-  }
-`;
+// const GET_ARTICLES = gql`
+//   query articles {
+//     articles {
+//       title
+//       description
+//     }
+//   }
+// `;
 
 const Home = () => {
-    const { loading, error, data } = useQuery(GET_ARTICLES);
-
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
-    if (data) return <Box>
-        <Typography variant={'h1'}>{data.articles[0].title}</Typography>
-        <Typography variant={'body1'}>{data.articles[0].description}</Typography>
-        </Box>
-
-    console.log(data)
+    // const { loading, error, data } = useQuery(GET_ARTICLES);
+    //
+    // if (loading) return <p>Loading...</p>;
+    // if (error) return <p>Error: {error.message}</p>;
+    // if (data) return <Box>
+    //     <Typography variant={'h1'}>{data.articles[0].title}</Typography>
+    //     <Typography variant={'body1'}>{data.articles[0].description}</Typography>
+    //     </Box>
+    //
+    // console.log(data)
   return (
       <Container>
             <Box>
-
+Hello!
             </Box>
       </Container>
   );
