1c972119facbaf54bb5e28def00431419bd53c4c
Oleh Grechukh <diamondsl.dg9@gmail.com>
2025-04-02 13:54:23 +0300

    WIP: DigitalOcean backend fix deployment issue

--- backend/config/env/production/database.ts
+++ backend/config/env/production/database.ts
@@ -4,9 +4,12 @@ const {host, port, database, user, password} = parse(
     process.env.DATABASE_URL
 );
 
+console.info(process.env.DATABASE_URL)
+
 export default ({env}) => ({
     connection: {
         client: 'postgres',
+        connectionString: `postgresql://doadmin:${process.env.DATABASE_PASSWORD}@db-postgresql-fra1-54008-do-user-20170373-0.i.db.ondigitalocean.com:25060/defaultdb?sslmode=require`,
         connection: {
             host: host,
             port: port,
