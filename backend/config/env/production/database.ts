1eda3d0bbab814859b54d9441e5350a17c2e47b6
Oleh Grechukh <diamondsl.dg9@gmail.com>
2025-04-02 14:04:42 +0300

    WIP: DigitalOcean backend fix deployment issue

--- backend/config/env/production/database.ts
+++ backend/config/env/production/database.ts
@@ -9,15 +9,15 @@ console.info(process.env.DATABASE_URL)
 export default ({env}) => ({
     connection: {
         client: 'postgres',
-        connectionString: `postgresql://doadmin:${process.env.DATABASE_PASSWORD}@db-postgresql-fra1-54008-do-user-20170373-0.i.db.ondigitalocean.com:25060/defaultdb?sslmode=require`,
+        //connectionString: `postgresql://doadmin:${process.env.DATABASE_PASSWORD}@db-postgresql-fra1-54008-do-user-20170373-0.i.db.ondigitalocean.com:25060/defaultdb?sslmode=require`,
         connection: {
             host: host,
             port: port,
             database: database,
             user: user,
             password: password,
-            ssl: {
-                rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+            ssl: env.bool('DATABASE_SSL', true) && {
+                rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
                 ca: env('DATABASE_CA'),
             },
 
