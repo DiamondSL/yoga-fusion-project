93bd4ba799e2ed25b1b2ace4df48bea5e850836a
Oleh Grechukh <diamondsl.dg9@gmail.com>
2025-04-02 14:53:01 +0300

    WIP: DigitalOcean backend fix deployment issue

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,39 +1,42 @@
+export default ({env}) => {
+    const client = env('DATABASE_CLIENT', 'postgres');
+    const host = env('DATABASE_HOST', process.env.DATABASE_HOST);
+    const port = env('DATABASE_PORT', process.env.DATABASE_PORT);
+    const user = env('DATABASE_USER', process.env.DATABASE_USER);
+    const password = env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD);
+    const database = env('DATABASE_NAME', process.env.DATABASE_NAME);
+    const databaseURL = env('DATABASE_URL', process.env.DATABASE_URL);
 
-export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
-  const host = env('DATABASE_HOST', process.env.DATABASE_HOST);
-  const port = env('DATABASE_PORT', process.env.DATABASE_PORT);
-  const user = env('DATABASE_USER', process.env.DATABASE_USER);
-  const password = env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD);
-  const database = env('DATABASE_NAME', process.env.DATABASE_NAME);
+    console.info(databaseURL)
 
 
-  const connections = {
-    postgres: {
-      connection: {
-        host: env('DATABASE_HOST', host),
-        port: env.int('DATABASE_PORT', port),
-        database: env('DATABASE_NAME', database),
-        user: env('DATABASE_USERNAME', user),
-        password: env('DATABASE_PASSWORD', password),
-        ssl: env.bool('DATABASE_SSL', true) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+    const connections = {
+        postgres: {
+            connectionString: databaseURL,
+            connection: {
+                host: env('DATABASE_HOST', host),
+                port: env.int('DATABASE_PORT', port),
+                database: env('DATABASE_NAME', database),
+                user: env('DATABASE_USERNAME', user),
+                password: env('DATABASE_PASSWORD', password),
+                ssl: env.bool('DATABASE_SSL', true) && {
+                    key: env('DATABASE_SSL_KEY', undefined),
+                    cert: env('DATABASE_SSL_CERT', undefined),
+                    ca: env('DATABASE_CA', undefined),
+                    capath: env('DATABASE_SSL_CAPATH', undefined),
+                    cipher: env('DATABASE_SSL_CIPHER', undefined),
+                    rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+                },
+            },
+            pool: {min: env.int('DATABASE_POOL_MIN', 0), max: env.int('DATABASE_POOL_MAX', 10)},
         },
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-  };
+    };
 
-  return {
-    connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-    },
-  };
+    return {
+        connection: {
+            client,
+            ...connections[client],
+            acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+        },
+    };
 };
